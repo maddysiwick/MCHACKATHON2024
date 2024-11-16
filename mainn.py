bf5f1e195dbd643cb17704ad204e16dd45747579
maddysiwick <maddy.siwick@icloud.com>
2024-11-16 11:38:46 -0500

    added main loop

---
+++ mainn.py
@@ -0,0 +1,14 @@
+import math
+moveTime=0 #second
+statTime=0
+v=5830 #m/s
+c=3*10**8
+gamma = 1/math.sqrt(1-(v**2/c**2))
+
+
+while statTime <= 100:
+    moveTime+=1
+    statTime+=gamma*1
+
+print('move time ', moveTime)
+print('stat time ', statTime)
