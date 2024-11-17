063c3e9bab510e1b97342ebd8f34cc6f2b157b0f
maddysiwick <maddy.siwick@icloud.com>
2024-11-17 11:00:00 -0500

    the main ui is done

--- mainn.py
+++ mainn.py
@@ -2,6 +2,7 @@ import math
 import time
 from graphics import *
 
+
 def checkAge(age):
     if 90.0<=age:
         return 9
@@ -36,12 +37,15 @@ def main():
     win.setBackground(color_rgb(19,28,62))
     
 
-    label = Text(Point(280,100), "Speed of the Kathy mobile:")
+    label = Text(Point(380,100), "Speed of the Kathy mobile:")
     label.setSize(20)
     label.setTextColor("white")
     label.draw(win)
-
-    noworkkathy = Text(Point(450,150),"The Kathy mobile cannot run at this speed, KATHY SLOW DOWN!")
+    earth=Image(Point(200,825),'earth.png')
+    earth.draw(win)
+    ship=Image(Point(1050,675),'kathymobile.png')
+    ship.draw(win)
+    noworkkathy = Text(Point(550,150),"The Kathy mobile cannot run at this speed, KATHY SLOW DOWN!")
     noworkkathy.setSize(20)
     noworkkathy.setTextColor("white")
 
@@ -54,7 +58,7 @@ def main():
         return Submitbutton
 
     #buttons()
-    button = buttons (500,200)
+    button = buttons (600,200)
     
     
 
@@ -62,7 +66,8 @@ def main():
 
     #create our objects
 
-    input_box = Entry(Point(520,100),30)
+    input_box = Entry(Point(700,100),30)
+    input_box.setSize(20)
     input_box.setFill('white')
     input_box.draw(win)
     #txt = Text(Point(370,280),"")
@@ -73,7 +78,7 @@ def main():
        
        click_point = win.getMouse()
 
-       if 500 <= click_point.getX() <= 530 and 170 <= click_point.getY() <=200:
+       if 600 <= click_point.getX() <= 630 and 170 <= click_point.getY() <=200:
             break 
        
     
@@ -82,37 +87,55 @@ def main():
 
     my_txt = input_box.getText()
 
-    yesworkkathy = Text(Point(450,150), "Kathy is now speeding around the Earth at " + my_txt + " km/s")
+    yesworkkathy = Text(Point(600,150), "Kathy is now speeding around the Earth at " + my_txt + " m/s")
     yesworkkathy.setSize(20)
     yesworkkathy.setTextColor("white")
     
     if int(my_txt) > 300000000:
         noworkkathy.draw(win)
     else:
-        print ("The speed of the Kathy mobile is " + my_txt + " km/s")
+        print ("The speed of the Kathy mobile is " + my_txt + " m/s")
         yesworkkathy.draw(win)
         v=int(my_txt)
 
     if v>0:
         allImages=['kathybaby.PNG','kathy5.PNG','kathy10.PNG','kathy15.PNG','kathy25.PNG','kathy35.PNG','kathy45.PNG','kathy60.PNG','kathy75.PNG','kathydead.PNG']
-        moveTime=0.1 #second
+        moveTime=0.0 #second
         statTime=0.0
         c=3.0*10**8
         gamma = 1/math.sqrt(1-(v**2/c**2))
         count=0
         efile=0
         sfile=0
+        elabel=Text(Point(200,200),"Karth's age:")
+        elabel.setSize(20)
+        elabel.setTextColor('white')
+        slabel=Text(Point(1000,200),"Kathy's age:")
+        slabel.setSize(20)
+        slabel.setTextColor('white')
         imgEarth=Image(Point(200,500),allImages[efile])
-        imgSpace=Image(Point(800,500),allImages[sfile])
+        imgSpace=Image(Point(1000,500),allImages[sfile])
         imgEarth.draw(win)
         imgSpace.draw(win)
-        while statTime <= 100:
+        slabel.draw(win)
+        elabel.draw(win)
+        while statTime <= 90:
             count+=1
             time.sleep(1)
             moveTime+=1
             statTime+=gamma*1
             print(statTime)
             if count>1:
+                eage.undraw()
+                sage.undraw()
+                eage=Text(Point(200,250),int(statTime))
+                eage.setSize(20)
+                eage.setTextColor('white')
+                sage=Text(Point(1000,250),int(moveTime))
+                sage.setSize(20)
+                sage.setTextColor('white')
+                eage.draw(win)
+                sage.draw(win)
                 eprev=efile
                 sprev=sfile
                 efile=checkAge(statTime)
@@ -123,11 +146,20 @@ def main():
                     imgEarth.draw(win)
                 if sprev!=sfile:
                     imgSpace.undraw()
-                    imgSpace=Image(Point(800,500),allImages[sfile])
+                    imgSpace=Image(Point(1000,500),allImages[sfile])
                     imgSpace.draw(win)
             else:
+                eage=Text(Point(200,250),int(statTime))
+                eage.setSize(20)
+                eage.setTextColor('white')
+                sage=Text(Point(1000,250),int(moveTime))
+                sage.setSize(20)
+                sage.setTextColor('white')
+                eage.draw(win)
+                sage.draw(win)
                 efile=checkAge(statTime)
                 sfile=checkAge(moveTime)
+        
 
     win.getMouse()
     win.close()
